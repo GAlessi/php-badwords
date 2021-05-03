cfbe73b276feb9ae55b8e88b5bbd007118596ce0
Gabriele Alessi <89archaon89@gmail.com>
2021-05-03 18:02:44 +0200

    Esercizio modifico per nuova consegna

--- index.php
+++ index.php
@@ -6,12 +6,45 @@
     </head>
     <body>
 
+        <style media="screen">
+            .container{
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                width: 100%;
+                height: 100vh;
+            }
+
+            h2{
+                margin-bottom: 50px;
+            }
+
+
+        </style>
+
         <?php
             $sentence = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
             $badword = $_GET['badword'];
             $censuredSentence = str_replace($badword, "***", $sentence);
-            $strLength = strlen($censuredSentence);
-            echo "$censuredSentence   $strLength";
         ?>
+
+
+        <h1>Paragrafo originale</h1>
+        <p>
+            <?php echo "$sentence"; ?>
+        </p>
+        <h2>Lunghezza paragrafo originale:
+            <?php echo strlen("$sentence"); ?>
+        </h2>
+
+        <h1>Paragrafo censurato</h1>
+        <p>
+            <?php echo "$censuredSentence"; ?>
+        </p>
+        <h2>Lunghezza paragrafo originale:
+            <?php echo strlen($censuredSentence); ?>
+        </h2>
+
     </body>
 </html>
